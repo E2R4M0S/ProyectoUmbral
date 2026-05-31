bbff2b4e7c648768af8f03ce3d76335f6c030b6a
Adrian <adriancereijo1@gmail.com>
2026-05-30 21:06:58 -0400

    chore(infra): Postgres provider, EF.Design and runtime deps (libgssapi) and DI

---
+++ src/Trivia.Service/Trivia.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System.Reflection;
+
+namespace Trivia.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<TriviaDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        // Register MediatR handlers located in Infrastructure
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
---
+++ src/Trivia.Service/Trivia.Infrastructure/TriviaDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Trivia.Infrastructure;
+
+public class TriviaDbContext : DbContext
+{
+    public TriviaDbContext(DbContextOptions<TriviaDbContext> options) : base(options)
+    {
+    }
+
+    // Domain sets
+    public DbSet<Domain.Quiz>? Quizzes { get; set; }
+    public DbSet<Domain.Question>? Questions { get; set; }
+    public DbSet<Domain.Answer>? Answers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Domain.Quiz>(b =>
+        {
+            b.HasKey(x => x.Id);
+            b.HasIndex(x => x.Name).IsUnique();
+            b.Property(x => x.Name).IsRequired().HasMaxLength(200);
+            b.Property(x => x.Description).HasMaxLength(2000);
+            b.HasMany(x => x.Questions).WithOne(q => q.Quiz).HasForeignKey(q => q.QuizId).OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<Domain.Question>(b =>
+        {
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Text).IsRequired().HasMaxLength(2000);
+            b.HasMany(x => x.Answers).WithOne(a => a.Question).HasForeignKey(a => a.QuestionId).OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<Domain.Answer>(b =>
+        {
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Text).IsRequired().HasMaxLength(1000);
+        });
+    }
+
+    // Ensure DB created when starting (for development only)
+    public void EnsureDatabaseCreated()
+    {
+        Database.EnsureCreated();
+    }
+}
