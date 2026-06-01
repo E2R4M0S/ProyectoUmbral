8baf8fc50c53c046d284eef001bc6b6be055f560
Adrian <adriancereijo1@gmail.com>
2026-06-01 12:28:06 -0400

    fix(messaging): use IServiceProvider.CreateScope via DI for leaderboard updates

--- src/Trivia.Service/Trivia.Infrastructure/Messaging/RabbitMQ/TriviaAnswerSubmittedConsumer.cs
+++ src/Trivia.Service/Trivia.Infrastructure/Messaging/RabbitMQ/TriviaAnswerSubmittedConsumer.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
 
 namespace Trivia.Infrastructure.Messaging.RabbitMQ;
