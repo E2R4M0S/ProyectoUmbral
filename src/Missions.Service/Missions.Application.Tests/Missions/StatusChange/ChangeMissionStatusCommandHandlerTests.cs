71ec7ca46b497200e2681655bf03111b76030552
E2R4M0S <eadosramos.23@est.ucab.edu.ve>
2026-06-04 19:13:13 -0400

    Revert "test: agrega pruebas para Proxy, Chain of Responsibility y Facade"
    This reverts commit 50196d7e06153fe7211741f488532131a51b0110.

--- src/Missions.Service/Missions.Application.Tests/Missions/StatusChange/ChangeMissionStatusCommandHandlerTests.cs
+++ src/Missions.Service/Missions.Application.Tests/Missions/StatusChange/ChangeMissionStatusCommandHandlerTests.cs
@@ -48,7 +48,6 @@ public class ChangeMissionStatusCommandHandlerTests
 
         var existingMission = Mission.Create(
             "Test", "Test", Difficulty.Easy, 30, MissionType.Treasure);
-        existingMission.AddStage("Etapa 1", "Desc", 1);
 
         _repository.GetByIdAsync(missionId, Arg.Any<CancellationToken>())
             .Returns(existingMission);
